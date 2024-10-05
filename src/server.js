736def8c37c8ffb39ac0a68c3c6150d5198a3167
ptrodion <rdnpetrenko@gmail.com>
2024-10-05 16:07:57 +0100

    added status

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -39,7 +40,8 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
-      message: `A completely known contact with the id ${id}!`,
+      status: 200,
+      message: `Successfully found contact with id {contactId}!`,
       data: contact,
     });
   });
