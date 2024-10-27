036b8ea6d3691a5ecd3201222c946404c0fe8665
ptrodion <rdnpetrenko@gmail.com>
2024-10-27 08:34:48 +0000

    optimized the code, removed unnecessary imports

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -1,10 +1,9 @@
-import mongoose from 'mongoose';
 import { model, Schema } from 'mongoose';
 
 const sessionSchema = new Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       required: true,
     },
     accessToken: {
